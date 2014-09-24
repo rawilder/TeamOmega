4af7d4ccddb3f73337f314d8040a4f1ee8a01f37
msnyder102 <msnyder102@gmail.com>
2014-09-24 14:16:04 -0500

    Basic Game Stuff

---
+++ OmegaGolf/Assets/TestScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour
+{
+
+
+    Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector3(0,0,0);
+            if (Physics.Raycast(r, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.CompareTag("Ball"))
+                {
+                    Vector3 dir = transform.position - hit.point; 
+                    rb.AddForce(dir.x * 100.0f, 0, dir.z * 100.0f);
+                }
+            }
+        }
+    }
+}
