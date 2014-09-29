dced9b748aaaf8368f349a61aa737132cda10d63
msnyder102 <msnyder102@gmail.com>
2014-09-29 13:11:00 -0500

    Added mechanics to shoot golf ball with an arrow

--- OmegaGolf/Assets/BallBehavior.cs
+++ OmegaGolf/Assets/BallBehavior.cs
@@ -17,21 +17,21 @@ public class BallBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Input.GetMouseButtonDown(0))
-        {
-            rb.velocity = new Vector3(0,0,0);
-            if (Physics.Raycast(r, out hit, Mathf.Infinity))
-            {
-                if (hit.collider.gameObject.CompareTag("Ball"))
-                {
-                    Vector3 dir = transform.position - hit.point; 
-                    rb.AddForce(dir.x * 1000.0f, 0, dir.z * 1000.0f);
-                }
-            }
-        }
-
+//        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+//        RaycastHit hit;
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            rb.velocity = new Vector3(0,0,0);
+//            if (Physics.Raycast(r, out hit, Mathf.Infinity))
+//            {
+//                if (hit.collider.gameObject.CompareTag("Ball"))
+//                {
+//                    Vector3 dir = transform.position - hit.point; 
+//                    rb.AddForce(dir.x * 1000.0f, 0, dir.z * 1000.0f);
+//                }
+//            }
+//        }
+//
         if(rb.position.y < -6)
         {
             rb.velocity = Vector3.zero;
---
+++ OmegaGolf/Assets/BallMovement.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallMovement : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Ray _r;
+    private RaycastHit _hit;
+    private Plane ballPlane;
+    private Transform _myTransform;
+    private Vector3 _lastAimPoint;
+    private int _frameCounter;
+
+    public float speedFactor = 100.0f;  //variable ball speed  
+    public float minInputRange = 0.25f;
+    public float maxInputRange = 5.0f;     //variable input circle radius
+    public float speedThreshold = 0.1f; //when ball speed drops below this val it is ready to be shot again
+
+    public enum BallState
+    {
+        moving,
+        waiting,
+        aiming
+    };
+
+    private BallState bState;
+
+    public GameObject arrowSprite;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _myTransform = transform;
+        bState = BallState.moving;
+        ballPlane = new Plane();
+        _frameCounter = 0;
+        arrowSprite.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (bState)
+        {
+            case BallState.moving:
+                //wait for ball to stop moving for 30 frames (1/2 second) before allowing player to do input
+                CheckForBallStop();
+                break;
+            case BallState.waiting:
+                //wait for player to click ball before doing aiming actions
+                CheckForInput();
+                break;
+            case BallState.aiming:
+                //draw line for force/direction on mousposition, apply force on mouseup
+                Aiming();
+                break;
+        }
+    }
+
+    private void CheckForBallStop()
+    {
+        if (_rb.velocity.magnitude <= speedThreshold)
+        {
+            _frameCounter++;
+            if (_frameCounter >= 30)
+            {
+                bState = BallState.waiting;
+                _rb.Sleep();
+            }
+        }
+        else
+        {
+            _frameCounter = 0;
+        }
+    }
+
+    private void CheckForInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(_r, out _hit, Mathf.Infinity))
+            {
+                if (_hit.collider.gameObject.CompareTag("Ball"))
+                {
+                    ballPlane.SetNormalAndPosition(Vector3.up, _myTransform.position);
+                    bState = BallState.aiming;
+                    //reset arrow position
+                    arrowSprite.transform.position = _myTransform.position;
+                    arrowSprite.transform.localScale = new Vector3(0, 0, 0);
+                    arrowSprite.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void Aiming()
+    {
+        //Draw line from ball center to current mouse position
+        _r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayDist;
+        if (ballPlane.Raycast(_r, out rayDist))
+        {
+            Vector3 pos = _r.GetPoint(rayDist);
+            if (Vector3.Distance(_myTransform.position, pos) < minInputRange)
+            {
+                //calculate unit vector in direction of mouse, give min input range in that direction
+                Vector3 dir = pos - _myTransform.position;
+                _lastAimPoint = _myTransform.position + minInputRange * dir.normalized;
+            }
+            else if (Vector3.Distance(_myTransform.position, pos) < maxInputRange)
+            {
+                _lastAimPoint = pos;
+            }
+            else
+            {
+                //calculate unit vector in direction of mouse, give max input range in that direction
+                Vector3 dir = pos - _myTransform.position;
+                _lastAimPoint = _myTransform.position + maxInputRange * dir.normalized;
+            }
+            //rotate arrow to face ball
+            arrowSprite.transform.position = _lastAimPoint + (_lastAimPoint - _myTransform.position).normalized / 2;
+            arrowSprite.transform.rotation = Quaternion.Euler(0, 90, 0) * Quaternion.LookRotation(Vector3.up, _myTransform.position - _lastAimPoint);
+            //scale arrow to proper size
+            float mag = (_lastAimPoint - _myTransform.position).magnitude;
+            arrowSprite.transform.localScale = new Vector3(mag, mag / 2, 1);
+        }
+
+
+        //check for mouse up, if true apply force
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 dir = _myTransform.position - _lastAimPoint;
+            _rb.WakeUp();
+            _rb.AddForce(dir.x * speedFactor, 0, dir.z * speedFactor);
+            bState = BallState.moving;
+            arrowSprite.SetActive(false);
+        }
+    }
+}
