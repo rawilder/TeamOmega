fa8208a0ce35629a2a7b5b110543f7edb3d525e9
Marcos Alfonso <marcostocoast@gmail.com>
2014-10-04 17:11:47 -0500

    Started work on editable parts of level

---
+++ OmegaGolf/Assets/EditableEntity.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditableEntity : MonoBehaviour {
+
+    bool drawEditor;
+
+	// Use this for initialization
+	void Start () {
+	    drawEditor = false;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+ 
+            if (Physics.Raycast(r, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.CompareTag("Editable"))
+                {
+                    drawEditor = !drawEditor;
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (drawEditor)
+        {
+            GUI.Box(new Rect(675, 50, 200, 75),"Edit Box");
+        }
+    }
+}
