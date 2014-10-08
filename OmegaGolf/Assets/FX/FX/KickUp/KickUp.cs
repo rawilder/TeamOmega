029bd47917b499987831604032b1c9287b2d4d04
msnyder102 <msnyder102@gmail.com>
2014-10-08 11:28:37 -0500

    FX
    Glow, Trail, Kickup, Smoke

---
+++ OmegaGolf/Assets/FX/FX/KickUp/KickUp.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KickUp : MonoBehaviour
+{
+
+    private Transform _myTransform;
+    private Rigidbody _ballBody;
+    private Transform _ball;
+    private Vector3 _ballOffset;
+    private ParticleSystem _ps;
+    private float _rate;
+    private float _speed;
+    public float speedThreshold;
+
+    // Use this for initialization
+    void Start()
+    {
+        _myTransform = transform;
+        _ball = _myTransform.parent;
+        _ballBody = _ball.gameObject.GetComponent<Rigidbody>();
+        _ballOffset = new Vector3(0, .4f, 0);
+        _ps = GetComponent<ParticleSystem>();
+        _rate = _ps.emissionRate;
+        _speed = _ps.startSpeed;
+    }
+
+
+    void Update()
+    {
+        _myTransform.position = _ball.position - _ballOffset;
+        _myTransform.rotation = Quaternion.Euler(-20,0,0) * Quaternion.FromToRotation(Vector3.forward, -_ballBody.velocity);
+        if(_ballBody.velocity.magnitude >= speedThreshold)
+        {
+            _ps.emissionRate = _rate * _ballBody.velocity.magnitude;
+            _ps.startSpeed = _speed * _ballBody.velocity.magnitude;
+        }
+        else
+        {
+            _ps.emissionRate = 0;
+            _ps.startSpeed = 0;
+        }
+    }
+}
---
+++ OmegaGolf/Assets/FX/TestHover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestHover : MonoBehaviour {
+
+
+    private ParticleSystem _ps;
+    private float _rate;
+
+	// Use this for initialization
+	void Start () {
+        _ps = GetComponentInChildren<ParticleSystem>();
+        _rate = _ps.emissionRate;
+        _ps.emissionRate = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
+        if(Physics.Raycast(r, out _hit, Mathf.Infinity) && _hit.collider.gameObject == gameObject)
+        {
+            _ps.emissionRate = _rate;
+        }
+        else
+        {
+            _ps.emissionRate = 0;
+        }
+	}
+}
