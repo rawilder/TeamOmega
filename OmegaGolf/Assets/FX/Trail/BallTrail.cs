87c3de80a2ec6c6e3484a6a5934e9f640ff0af1e
MarcosAlfonso <marcostocoast@gmail.com>
2014-11-17 13:57:24 -0600

    Finally got matt's FX in

--- OmegaGolf/Assets/FX/Trail/BallTrail.cs
+++ OmegaGolf/Assets/FX/Trail/BallTrail.cs
@@ -10,17 +10,38 @@ public class BallTrail : MonoBehaviour {
     private Transform _myTransform;
     private Quaternion _rot;
     private Transform _ball;
+    private Rigidbody _ballBody;
+    private ParticleSystem _ps;
+    public float emitRate = 100f;
 
 	// Use this for initialization
 	void Start () {
+
         _myTransform = transform;
         _rot = transform.rotation;
         _ball = _myTransform.parent;
+
+        _ballBody = _ball.gameObject.GetComponent<Rigidbody>();
+        _ps = GetComponent<ParticleSystem>();
+        
+        _ps.emissionRate = emitRate;
+
 	}
 	
 	
 	void LateUpdate () {
         _myTransform.position = _ball.position - new Vector3(0,.4f,0);
         _myTransform.rotation = _rot;
+
+        if(_ballBody.velocity.magnitude < 1)
+        {
+
+            _ps.emissionRate = 0;
+        }
+        else
+        {
+            Debug.Log("Emit here");
+            _ps.emissionRate = emitRate;
+        }
 	}
 }
